a076abe4a573dfe083567bbc5cc3f70f9222454f
Yujie Zhong <destiny950923@outlook.com>
2019-06-21 11:42:22 +0800

    Revert "m"
    This reverts commit ade946359a1ddd806379863ee73e8b526211ace6.

--- src/main.js
+++ src/main.js
@@ -1,15 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Router from 'vue-router'
-import router from './router'
-import element from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false
-Vue.use(Router)
-Vue.use(element)
 
 new Vue({
   render: h => h(App),
-  router,
 }).$mount('#app')
